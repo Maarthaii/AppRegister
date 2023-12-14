fd6ad7f3c0a2e8357a9da5ebb1954d7eabe0035b
Maarthaii <maar.thaii.2003@gmail.com>
2023-12-14 16:14:58 -0500

    Preparacion

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ const bodyParser = require('body-parser');
 
 //SERVIDOR Y PUERTO
 const server=express();
-const port=3000;
+//const port=3000;
+const port = process.env.PORT || 3000;
 
 //CONFIGURACIONES
 server.use(morgan('dev'));
